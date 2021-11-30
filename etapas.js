eeb72fa7985b9b9f84bb491d392357ee89802173
THONWELLING <thonwellingdani13@gmail.com>
2021-11-30 17:44:40 -0300

    definindo Json de candidatos

---
+++ etapas.js
@@ -0,0 +1,162 @@
+let etapas = [
+  {
+    title: 'HOKAGE',
+    numbers: 2,
+    candidates: [
+      {
+        number: 99,
+        name: 'Namikaze Minato',
+        cla: 'Namikaze',
+        vice: 'Uzumaki Kushina',
+        photos: [
+          {url: 'minato.jpg', subtitle: 'Hokage'},
+          {url: 'Kushina_uzumaki.jpg', subtitle: 'Vice-Hokage', small: true}
+        ]
+      },
+      {
+        number: 98,
+        name: 'Uzumaki Naruto',
+        cla: 'Uzumaki',
+        vice: 'Hyuga Hinata',
+        photos: [
+          {url: 'naruto.jpg', subtitle: 'Hokage'},
+          {url: 'hinata.jpg', subtitle: 'Vice-Hokage', small: true}
+        ]
+      },
+      {
+        number: 97,
+        name: 'Uchiha Sasuke',
+        cla: 'Uchiha',
+        vice: 'Haruno Sakura',
+        photos: [
+          {url: 'sasuke.jpg', subtitle: 'Hokage'},
+          {url: 'sakura.png', subtitle: 'Vice-Hokage', small: true}
+        ]
+      },
+      {
+        number: 96,
+        name: 'Hatake Kakashi',
+        cla: 'Hatake',
+        vice: 'Uchiha Obito',
+        photos: [
+          {url: 'hatake-kakashi.jpg', subtitle: 'Hokage'},
+          {url: 'obito.jpg', subtitle: 'Vice-Hokage', small: true}
+        ]
+      },
+      {
+        number: 95,
+        name: 'Senju Tsunade',
+        cla: 'Senju',
+        vice: 'Hatake Jiraiya',
+        photos: [
+          {url: 'tsunade.jpg', subtitle: 'Hokage'},
+          {url: 'jiraiya.jpeg', subtitle: 'Vice-Hokage', small: true}
+        ]
+      },
+      {
+        number: 94,
+        name: 'Uchiha Hitachi',
+        cla: 'Uchiha',
+        vice: 'Uchiha Shisui',
+        photos: [
+          {url: 'hitachi.jpg', subtitle: 'Hokage'},
+          {url: 'shisui.jpg', subtitle: 'Vice-Hokage', small: true}
+        ]
+      },
+      {
+        number: 93,
+        name: 'Sarutobi Hiruzen',
+        cla: 'Sarutobi',
+        vice: 'Shimura Danzou ',
+        photos: [
+          {url: 'Hiruzen_jovem.png', subtitle: 'Hokage'},
+          {url: 'danzou.jpg', subtitle: 'Vice-Hokage', small: true}
+        ]
+      },
+      {
+        number: 92,
+        name: 'Might Guy',
+        cla: 'Might',
+        vice: 'Rock Lee ',
+        photos: [
+          {url: 'gai.jpg', subtitle: 'Hokage'},
+          {url: 'rock-lee.jpeg', subtitle: 'Vice-Hokage', small: true}
+        ]
+      },
+      {
+        number: 91,
+        name: 'Orochimaru',
+        cla: 'Desconhecido',
+        vice: 'Yakushi Kabuto ',
+        photos: [
+          {url: 'orochimaru.png', subtitle: 'Hokage'},
+          {url: 'Kabuto_Yakushi.png', subtitle: 'Vice-Hokage', small: true}
+        ]
+      },
+      {
+        number: 90,
+        name: 'Uzumaki Nagato',
+        cla: 'Uzumaki',
+        vice: 'Yahiko ',
+        photos: [
+          {url: 'nagato.png', subtitle: 'Hokage'},
+          {url: 'yahiko.jpg', subtitle: 'Vice-Hokage', small: true}
+        ]
+      },
+    ]
+  },
+  {
+    title: 'Conselheiro',
+    numbers: 5,
+    candidates: [
+      {
+        number: 31128,
+        name: 'Nara Shikamaru',
+        cla: 'Nara',
+        photos: [
+          {url: 'shikamaru.jpg', subtitle: 'Conselheiro'},
+        ]
+      },
+      {
+        number: 21185,
+        name: 'Yamanaka Ino',
+        cla: 'Yamanaka',
+        photos: [
+          {url: 'ino.jpg', subtitle: 'Conselheira'},
+        ]
+      },
+      {
+        number: 42265,
+        name: 'Yamato',
+        cla: 'Desconhecido',
+        photos: [
+          {url: 'yamato.jpg', subtitle: 'Conselheiro'},
+        ]
+      },
+      {
+        number: 12607,
+        name: 'Hyuga Neji',
+        cla: 'Hyuga',
+        photos: [
+          {url: 'neji.jpg', subtitle: 'Conselheiro'},
+        ]
+      },
+      {
+        number: 22808,
+        name: 'Yamanaka Sai',
+        cla: 'Yamanaka',
+        photos: [
+          {url: 'hatake-kakashi.jpg', subtitle: 'Conselheiro'},
+        ]
+      },
+      {
+        number: 32154,
+        name: 'Dan Kato Shizune',
+        cla: 'Dan Kato',
+        photos: [
+          {url: 'shizune.jpg', subtitle: 'Conselheira'},
+        ]
+      }
+    ]
+  }
+]
